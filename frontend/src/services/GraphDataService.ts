4b212324c4dc92699ac1e75417cf590ee78ddc4e
Diogo Leite <diogoleite87@gmail.com>
2023-04-25 00:34:41 -0300

    feat: add generate and delete data graph service

--- frontend/src/services/GraphDataService.ts
+++ frontend/src/services/GraphDataService.ts
@@ -2,7 +2,11 @@ import { Api } from "../providers";
 import { GraphData } from "../schemas";
 
 const getGraphData = () => Api.get<GraphData>('/graph/all')
+const getGenerateAllData = () => Api.get('/generate')
+const getDeleteAllData = () => Api.get('/delete/all')
 
 export const GraphDataService = {
-    getGraphData
+    getGraphData,
+    getDeleteAllData,
+    getGenerateAllData
 }
