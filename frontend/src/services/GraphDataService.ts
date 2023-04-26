1519b3a6829b89bff61b405d0150d141fff279b0
Diogo Leite <diogoleite87@gmail.com>
2023-04-26 12:41:05 -0300

    feat: add requests for data service

--- frontend/src/services/GraphDataService.ts
+++ frontend/src/services/GraphDataService.ts
@@ -2,11 +2,13 @@ import { Api } from "../providers";
 import { GraphData } from "../schemas";
 
 const getGraphData = () => Api.get<GraphData>('/graph/all')
-const getGenerateAllData = () => Api.get('/generate')
+const getGenerateAllData = (lenght: number) => Api.get(`/generate/${lenght}`)
 const getDeleteAllData = () => Api.get('/delete/all')
+const getGraphDataLimit = (limit: number) => Api.get<GraphData>(`graph/${limit}`)
 
 export const GraphDataService = {
     getGraphData,
     getDeleteAllData,
-    getGenerateAllData
+    getGenerateAllData,
+    getGraphDataLimit
 }
