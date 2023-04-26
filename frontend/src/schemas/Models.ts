9c20d4bc1699a19eda4ea94b02a995330b520bc6
Diogo Leite <diogoleite87@gmail.com>
2023-04-26 12:41:27 -0300

    feat: add types for people, countr and city

--- frontend/src/schemas/Models.ts
+++ frontend/src/schemas/Models.ts
@@ -1,16 +1,35 @@
 export type GraphNode = {
-    id: string;
-    name: string;
-    age: number;
-    label: string;
+    id: string
+    name: string
+    age: number
+    label: string
 }
 
 export type GraphLink = {
-    source: string;
-    target: string;
+    source: string
+    target: string
+    label: string
 }
 
 export type GraphData = {
-    nodes: GraphNode[],
+    nodes: GraphNode[]
     links: GraphLink[]
+}
+
+export type People = {
+    id: number
+    name: string
+    age: number
+    gender: string
+    label: string
+}
+
+export type Country = {
+    id: number
+    name: string
+}
+
+export type City = {
+    id: number
+    name: string
 }
